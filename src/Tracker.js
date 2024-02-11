7ecf3f084fa597b1199d4d395b315bda6880bbc3
Adam Nguyen <101524918+AHNox@users.noreply.github.com>
2024-02-10 21:27:45 -0600

    Update Tracker.js

--- src/Tracker.js
+++ src/Tracker.js
@@ -1,22 +1,15 @@
 import Navbar from "./Navbar";
+import React from "react";
 const Tracker = () => {
-    class myHeader extends React.Component{
-        render(){
-            const myStyle = {
-                
-            }
-        }
-    }
-    return (
-        <div className="bg-[#F4B04E] min-h-screen">
-            <Navbar />
-             <p>Tracker</p>
-             <div class="flex mb-4">
-             <div class="w-1/2 border-b-2 border-customGray bg-gray-400 h-12 hover:underline-dashed"></div>
-             </div>
-
-        </div>
-    )
-}
+	return (
+		<div className="bg-[#F4B04E] min-h-screen">
+			<Navbar />
+			<p>Tracker</p>
+			<div class="flex mb-4">
+				<div class="w-1/2 border-b-2 border-customGray bg-gray-400 h-12 hover:underline-dashed"></div>
+			</div>
+		</div>
+	);
+};
 
 export default Tracker;
