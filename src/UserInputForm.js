c04d2bec3ae14d76bfe3e82410d1e08e8c685385
Adam Nguyen <ahnguyen12300@gmail.com>
2024-02-11 12:25:37 -0600

    Update UserInputForm.js

--- src/UserInputForm.js
+++ src/UserInputForm.js
@@ -1,18 +1,14 @@
 import React, { useState } from "react";
 
 const UserInputForm = () => {
-	// State to store the user input
 	const [inputValue, setInputValue] = useState("");
 
-	// Event handler for input change
 	const handleInputChange = (event) => {
 		setInputValue(event.target.value);
 	};
 
-	// Event handler for form submission
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		// Do something with the input value (e.g., send it to a server, update state, etc.)
 		console.log("User entered:", inputValue);
 	};
 
@@ -20,7 +16,6 @@ const UserInputForm = () => {
 		<form onSubmit={handleSubmit}>
 			<label>
 				Input COP Location:
-				{/* Input field with value and onChange controlled by state */}
 				<input
 					class="border-[#64422F] border-4 rounded-lg drop-shadow-xl bg-[#FFFBE5]"
 					type="text"
@@ -28,7 +23,6 @@ const UserInputForm = () => {
 					onChange={handleInputChange}
 				/>
 			</label>
-			{/* Button to submit the form */}
 			<button type="submit">Submit</button>
 		</form>
 	);
