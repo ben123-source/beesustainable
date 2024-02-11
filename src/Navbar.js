0e35e30a0c71ccd963d1efd9f20c89a21d8f3586
Adam Nguyen <ahnguyen12300@gmail.com>
2024-02-11 11:45:40 -0600

    Font changes and data dashboard start

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
 	const navigate = useNavigate();
 
 	return (
-		<div class="border-[#64422F] border-b-8 border-dashed drop-shadow-xl">
+		<div class="border-[#64422F] border-b-8 border-dashed drop-shadow-xl font-lucida text-lg">
 			<div className="bg-[#FFF3AC] w-screen h-14 flex justify-between items-center">
 				<div className="flex items-center h-full ml-2">
 					<img src={logo} alt="beelogo" className="h-1/2" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        customGray: '#64422F',
-      },
-    },
-  },
-  plugins: [
-    function ({ addUtilities, addVariant, e }) {
-      // Add a custom variant for hover state
-      addVariant('hover', ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => {
-          return `.${e(`hover${separator}${className}`)}:hover`;
-        });
-      });
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				customGray: "#64422F",
+			},
+			fontFamily: {
+				"lucida": [
+					"Lucida Console",
+					"Lucida Sans Typewriter",
+					"monaco",
+					"Bitstream Vera Sans Mono",
+					"monospace",
+				],
+			},
+		},
+	},
+	plugins: [
+		function ({ addUtilities, addVariant, e }) {
+			// Add a custom variant for hover state
+			addVariant("hover", ({ modifySelectors, separator }) => {
+				modifySelectors(({ className }) => {
+					return `.${e(`hover${separator}${className}`)}:hover`;
+				});
+			});
 
-      // Add custom utilities for dashed underline
-      addUtilities({
-        '.underline-dashed': {
-          textDecoration: 'underline',
-          textDecorationStyle: 'dashed',
-        }
-      }, ['hover']); // Apply this utility on hover
-    }
-  ],
-}
+			// Add custom utilities for dashed underline
+			addUtilities(
+				{
+					".underline-dashed": {
+						textDecoration: "underline",
+						textDecorationStyle: "dashed",
+					},
+				},
+				["hover"]
+			); // Apply this utility on hover
+		},
+	],
+};
+/*font-family: "Brush Script MT", cursive; font-size: 24px; font-style: normal; font-variant: normal; font-weight: 700; line-height: 26.4px; */
