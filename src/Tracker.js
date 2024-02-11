e90b3ae7549e88c77f8a3664a22149816b2166c1
Adam Nguyen <101524918+AHNox@users.noreply.github.com>
2024-02-11 03:01:46 -0600

    Created Wireframe

--- src/Tracker.js
+++ src/Tracker.js
@@ -2,18 +2,18 @@ import Navbar from "./Navbar";
 import React from "react";
 const Tracker = () => {
 	return (
-		<div className="bg-[#F4B04E] min-h-screen">
+		<div className="flex flex-col  bg-[#F4B04E] min-h-screen">
 			<Navbar />
-			<div class="">
-				<div class="flex px-28 py-12 justify-between gap-20 content-center">
-					<div class="basis-4/5 flex flex-col gap-5 text-center">
-						<div class="px-40 self-start bg-white grow-1 basis-14">
-							<div>word</div>
-						</div>
-						<div class="bg-white grow-4 basis-96">word</div>
+			<div class="flex grow basis-full px-28 py-12 justify-between gap-20 ">
+				<div class="basis-4/5 flex flex-col gap-5 text-center ">
+					<div class="px-40 self-start bg-white grow-1 basis-14 border-[#64422F] border-8 rounded-lg">
+						<div>title</div>
 					</div>
-					<div class="bg-white basis-1/4 text-center block"></div>
+					<div class="bg-white grow basis-96 border-[#64422F] border-8 rounded-lg">
+						map
+					</div>
 				</div>
+				<div class="bg-white basis-1/4 text-center block border-[#64422F] border-8 rounded-lg"></div>
 			</div>
 		</div>
 	);
