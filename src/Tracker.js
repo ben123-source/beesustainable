33f7a96b6b9050aa43c7ed941ef76bc19a8ab199
Ben Tuason <benjosedaa@gmail.com>
2024-02-11 11:53:29 -0600

    what

--- src/Tracker.js
+++ src/Tracker.js
@@ -1,12 +1,5 @@
 import Navbar from "./Navbar";
 const Tracker = () => {
-    class myHeader extends React.Component{
-        render(){
-            const myStyle = {
-                
-            }
-        }
-    }
     return (
         <div className="bg-[#F4B04E] min-h-screen">
             <Navbar />
@@ -14,7 +7,6 @@ const Tracker = () => {
              <div class="flex mb-4">
              <div class="w-1/2 border-b-2 border-customGray bg-gray-400 h-12 hover:underline-dashed"></div>
              </div>
-
         </div>
     )
 }
