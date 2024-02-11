539584fced89249af570248934db31d4442528d3
bentuason <benjosedaa@gmail.com>
2024-02-10 23:09:20 -0600

    wtf

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,28 +1,25 @@
-import { useNavigate } from "react-router-dom"
-import logo from './/bee.png'
+import { useNavigate } from "react-router-dom";
+import logo from './/bee.png';
 
 const Navbar = () => {
     const navigate = useNavigate();
 
     return(
         <div>
-            <div className='bg-[#FFF3AC] w-screen h-20  flex justify-between items-center'>
-            <img src={logo} alt="beelogo" className=" h-1/2"/>
-                <div className='flex items-center justify-center gap-4 ml-6'>
-                <button onClick={() => navigate("/")} className='hover:underline-dashed text-black hover:cursor-pointer'> BeeSustainable </button> 
+            <div className='bg-[#FFF3AC] w-screen h-14 flex justify-between items-center'>
+                <div className='flex items-center h-full ml-2'>
+                    <img src={logo} alt="beelogo" className="h-1/2"/>
+                    <button onClick={() => navigate("/")} className='hover:underline-dashed text-black hover:cursor-pointer ml-3'>BeeSustainable</button>
                 </div>
                 <div className='flex items-center justify-center gap-4 mr-6'>
-                <ul className='flex gap-6 mr-4'>
-                <button onClick={() => navigate("/")} className='hover:underline-dashed text-black hover:cursor-pointer'>Home</button>
-                <button onClick={() => navigate("/tracker")} className='hover:underline-dashed text-black hover:cursor-pointer'>Tracker</button>
-
-
-                </ul>
-                    
+                    <ul className='flex gap-6 mr-4'>
+                        <button onClick={() => navigate("/")} className='hover:underline-dashed text-black hover:cursor-pointer'>Home</button>
+                        <button onClick={() => navigate("/tracker")} className='hover:underline-dashed text-black hover:cursor-pointer'>Tracker</button>
+                    </ul>
                 </div>
             </div>
         </div>
     )
 }
 
-export default Navbar
+export default Navbar;
