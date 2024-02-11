1aa693e7914098e322b3ad69047315242f45b752
Adam Nguyen <ahnguyen12300@gmail.com>
2024-02-11 12:31:46 -0600

    Update UserInputForm.js

--- src/UserInputForm.js
+++ src/UserInputForm.js
@@ -1,27 +1,31 @@
 import React, { useState } from "react";
 
 const UserInputForm = () => {
-	const [inputValue, setInputValue] = useState("");
+	const [selectedOption, setSelectedOption] = useState("");
 
-	const handleInputChange = (event) => {
-		setInputValue(event.target.value);
+	const handleSelectChange = (event) => {
+		setSelectedOption(event.target.value);
 	};
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		console.log("User entered:", inputValue);
+		console.log("User selected:", selectedOption);
 	};
 
 	return (
 		<form onSubmit={handleSubmit}>
 			<label>
-				Input COP Location:
-				<input
-					class="border-[#64422F] border-4 rounded-lg drop-shadow-xl bg-[#FFFBE5]"
-					type="text"
-					value={inputValue}
-					onChange={handleInputChange}
-				/>
+				Select COP Location:
+				<select
+					className="border-[#64422F] border-4 rounded-lg drop-shadow-xl bg-[#FFFBE5]"
+					value={selectedOption}
+					onChange={handleSelectChange}>
+					<option value="">Select an option</option>
+					<option value="USA">United States</option>
+					<option value="CA">Canada</option>
+					<option value="AU">Austrilia</option>
+					<option value="N/UK">Norway/United Kingdom</option>
+				</select>
 			</label>
 			<button type="submit">Submit</button>
 		</form>
