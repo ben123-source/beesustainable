e2a2e14981c358b13dabc5e6460cb0acd1dc5b90
Adam Nguyen <101524918+AHNox@users.noreply.github.com>
2024-02-11 00:11:48 -0600

    Current tracker

--- src/Tracker.js
+++ src/Tracker.js
@@ -4,9 +4,16 @@ const Tracker = () => {
 	return (
 		<div className="bg-[#F4B04E] min-h-screen">
 			<Navbar />
-			<p>Tracker</p>
-			<div class="flex mb-4">
-				<div class="w-1/2 border-b-2 border-customGray bg-gray-400 h-12 hover:underline-dashed"></div>
+			<div class="">
+				<div class="flex px-28 py-12 justify-between gap-20 content-center">
+					<div class="basis-4/5 flex flex-col gap-5 text-center">
+						<div class="px-40 self-start bg-white grow-1 basis-14">
+							<div>word</div>
+						</div>
+						<div class="bg-white grow-4 basis-96">word</div>
+					</div>
+					<div class="bg-white basis-1/4 text-center block"></div>
+				</div>
 			</div>
 		</div>
 	);
